af4773de5446e464eb6309f50afb5f52c89ebe8d
Gerardo Abraham <geoabraham@tuta.io>
2023-01-04 01:56:53 -0300

    feat: add random fact from Wikipedia API

--- src/practical_hypermodern_python/console.py
+++ src/practical_hypermodern_python/console.py
@@ -1,6 +1,11 @@
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+
+API_URL = "https://es.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
@@ -9,4 +14,12 @@ def main():
     """
     Practical Hyper-modern Python
     """
-    click.echo("Hello World !!!")
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
