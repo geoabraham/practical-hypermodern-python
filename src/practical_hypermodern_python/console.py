ce37e0d6d845031ebf7a1a35de2cf13abafcdd22
Gerardo Abraham <geoabraham@tuta.io>
2023-01-06 02:53:03 -0300

    chore: move wikipedia to separate module

--- src/practical_hypermodern_python/console.py
+++ src/practical_hypermodern_python/console.py
@@ -1,11 +1,8 @@
 import textwrap
 
 import click
-import requests
 
-from . import __version__
-
-API_URL = "https://es.wikipedia.org/api/rest_v1/page/random/summary"
+from . import __version__, wikipedia
 
 
 @click.command()
@@ -14,9 +11,7 @@ def main():
     """
     Practical Hyper-modern Python
     """
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    data = wikipedia.random_page()
 
     title = data["title"]
     extract = data["extract"]
