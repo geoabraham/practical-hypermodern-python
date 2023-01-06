111708a5beecc9e4d7ec6458db1481819eb47c5e
Gerardo Abraham <geoabraham@tuta.io>
2023-01-06 03:04:22 -0300

    chore: handle exception

--- src/practical_hypermodern_python/wikipedia.py
+++ src/practical_hypermodern_python/wikipedia.py
@@ -1,9 +1,14 @@
+import click
 import requests
 
 API_URL = "https://es.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as err:
+        message = str(err)
+        raise click.ClickException(message)
