d72543e6d04e2aef7a3a3fda79881b9bfe4208e0
Gerardo Abraham <geoabraham@tuta.io>
2023-01-06 02:34:02 -0300

    test: add mock

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,11 +4,21 @@ import pytest
 from practical_hypermodern_python import console
 
 
+def test_main_succeeds(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
-    result = runner.invoke(console.main)
-    assert result.exit_code == 0
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Título mockeado",
+        "extract": "Bajada mockeada",
+    }
+    return mock
