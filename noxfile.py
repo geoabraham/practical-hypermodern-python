80de6c4ce27806edd710da9fa8a6dfa4ea3ad2c8
Gerardo Abraham <geoabraham@tuta.io>
2023-01-09 19:09:33 -0300

    chore: add type annotations noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
 import tempfile
+from typing import Any
 
 import nox
+from nox import Session
 
 nox.options.sessions = "lint", "mypy", "pytype", "safety", "tests"
 
@@ -9,14 +11,14 @@ PACKAGE = "practical_hypermodern_python"
 
 
 @nox.session(python="3.8")
-def black(session):
+def black(session) -> None:
     args = session.posargs or LOCATIONS
     install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python=["3.8", "3.7"])
-def lint(session):
+def lint(session) -> None:
     args = session.posargs or LOCATIONS
     install_with_constraints(
         session,
@@ -30,21 +32,21 @@ def lint(session):
 
 
 @nox.session(python=["3.8", "3.7"])
-def mypy(session):
+def mypy(session) -> None:
     args = session.posargs or LOCATIONS
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
 
 
 @nox.session(python="3.7")
-def pytype(session):
+def pytype(session) -> None:
     args = session.posargs or ["--disable=import-error", *LOCATIONS]
     install_with_constraints(session, "pytype")
     session.run("pytype", *args)
 
 
 @nox.session(python="3.8")
-def safety(session):
+def safety(session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -60,7 +62,7 @@ def safety(session):
 
 
 @nox.session(python=["3.8", "3.7"])
-def tests(session):
+def tests(session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
@@ -70,14 +72,14 @@ def tests(session):
 
 
 @nox.session(python=["3.8", "3.7"])
-def typeguard(session):
+def typeguard(session) -> None:
     args = session.posargs or ["-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
     session.run("pytest", f"--typeguard-packages={PACKAGE}", *args)
 
 
-def install_with_constraints(session, *args, **kwargs):
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
