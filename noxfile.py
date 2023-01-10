368ffe3c18a0c7fe8afb9966226879bf6d736f3c
Gerardo Abraham <geoabraham@tuta.io>
2023-01-10 15:06:24 -0300

    chore: add pytest-pspec

--- noxfile.py
+++ noxfile.py
@@ -70,10 +70,10 @@ def safety(session: Session) -> None:
 @nox.session(python=["3.8", "3.7"])
 def tests(session: Session) -> None:
     """Run the test suite."""
-    args = session.posargs or ["--cov", "-m", "not e2e"]
+    args = session.posargs or ["--cov", "-m", "not e2e", "--pspec"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock", "pytest-pspec"
     )
     session.run("pytest", *args)
 
